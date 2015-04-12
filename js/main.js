84166365b0ac252a6c63cd0d17ebb1a4c10e8fb7
Jose Zamudio <zamudio@outlook.com>
2015-04-11 17:42:33 -0700

    Add wordnik service

--- js/main.js
+++ js/main.js
@@ -2,20 +2,21 @@ angular.module('Variables', ['ui.router', 'ngLodash', 'ngClipboard'])
 .config(['ngClipProvider', function(ngClipProvider) {
   ngClipProvider.setPath('bower_components/zeropclipboard/dist/ZeroClipboard.swf');
 }])
-.controller('HomeController', ['$scope', 'lodash', function($scope, lodash) {
-  var str = 'Occaecat mollit sit enim ex reprehenderit id ut veniam et do cupidatat dolor aliqua Sit ad veniam Lorem voluptate consequat anim commodo Aliqua est occaecat ad sint consectetur incididunt consequat Eiusmod magna pariatur laborum reprehenderit pariatur adipisicing occaecat esse dolore officia Ullamco do tempor aliqua magna ex ipsum consectetur elit Esse mollit ut sint aute incididunt ex ex exercitation eu exercitation ea aute pariatur est Occaecat esse nostrud eiusmod tempor et Deserunt proident amet do mollit id excepteur quis et officia dolor Cupidatat laborum enim voluptate ipsum irure ex nisi deserunt magna'
-  $scope.words = str.split(' ');
-
+.controller('HomeController', ['$scope', 'lodash', 'Words', function($scope, lodash, Words) {
   $scope.generateRandom = function() {
     var temp = '';
-    for (var i = 0; i < $scope.number; i++) {
-      var random = Math.floor((Math.random() * $scope.words.length));
-      temp += ' ' + $scope.words[random];
-    }
+    Words.getWords($scope.number)
+    .then(function(response) {
+      console.log(response);
+      response.data.forEach(function(e) {
+        console.log(e);
+        temp += ' ' + e.word;
+      });
 
-    $scope.camel = lodash.camelCase(temp);
-    $scope.kebab = lodash.kebabCase(temp);
-    $scope.snake = lodash.snakeCase(temp);
+      $scope.camel = lodash.camelCase(temp);
+      $scope.kebab = lodash.kebabCase(temp);
+      $scope.snake = lodash.snakeCase(temp);
+    });
   };
 
   $scope.clear = function() {
